84b73f2f7643b2af3cc30cbdd9f025c83aa24531
Zach <zmeriksen@icloud.com>
2024-03-07 18:48:20 -0700

    Improve preview API (#1)
    * Improve preview API
    
    * Add workflow folder
    
    * Update docc.yml
    
    * Create ubuntu.yml
    
    * Delete .github/workflows/.github/workflows directory
    
    * Create ubunutu.yml
    
    * Create windows.yml
    
    * Rename ubunutu.yml to ubuntu.yml
    
    * Create macOS.yml
    
    * Create FUNDING.yml
    
    * Delete Package.resolved
    
    * Create ApplicationLogger.swift
    
    * Add tests
    
    * Remove mirror comments
    
    * Update gitignore
    
    * Remove .resolved

--- Tests/AppDependencyTests/AppDependencyTests.swift
+++ Tests/AppDependencyTests/AppDependencyTests.swift
@@ -1,12 +1,81 @@
 import XCTest
 @testable import AppDependency
 
-final class AppDependencyTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+fileprivate protocol Networking {
+    func fetch()
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+fileprivate class NetworkService: Networking {
+    func fetch() {
+        fatalError()
+    }
+}
+
+fileprivate class MockNetworking: Networking {
+    func fetch() { /* no-op */ }
+}
+
+fileprivate class ComposableService {
+    let networking: Networking
+
+    init(networking: Networking) {
+        self.networking = networking
+    }
+}
+
+fileprivate extension Application {
+    var networking: Dependency<Networking> {
+        dependency(NetworkService())
+    }
+
+    var composableService: Dependency<ComposableService> {
+        dependency(ComposableService(networking: Application.dependency(\.networking)))
+    }
+}
+
+fileprivate struct ExampleDependencyWrapper {
+    @AppDependency(\.networking) private var networking
+
+    func fetch() {
+        networking.fetch()
+    }
+}
+
+final class AppDependencyTests: XCTestCase {
+    override class func setUp() {
+        Application.logging(isEnabled: true)
+    }
+
+    override class func tearDown() {
+        Application.logger.debug("AppDependencyTests \(Application.description)")
+    }
+
+    func testComposableDependencies() {
+        let dependencyOverride = Application.override(\.networking, with: MockNetworking())
+
+        let composableService = Application.dependency(\.composableService)
+        composableService.networking.fetch()
+
+        dependencyOverride.cancel()
+    }
+
+    func testDependency() async throws {
+        let networkingOverride = Application.override(\.networking, with: MockNetworking())
+
+        let mockNetworking = Application.dependency(\.networking)
+
+        XCTAssertNotNil(mockNetworking as? MockNetworking)
+
+        mockNetworking.fetch()
+
+        let example = ExampleDependencyWrapper()
+
+        example.fetch()
+
+        networkingOverride.cancel()
+
+        let networkingService = Application.dependency(\.networking)
+
+        XCTAssertNotNil(networkingService as? NetworkService)
     }
 }
